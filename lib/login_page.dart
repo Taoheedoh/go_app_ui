2a2694540bcdcd085fd54fb9e1f0fa4aa83ca688
Taoheedoh <taoheedoh@gmail.com>
2024-09-08 22:32:48 +0100

    complete Login/register page

---
+++ lib/login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LOGIN extends StatefulWidget {
+  const LOGIN({super.key});
+
+  @override
+  State<LOGIN> createState() => _LOGINState();
+}
+
+class _LOGINState extends State<LOGIN> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -16,14 +16,106 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Go App',
       home: Scaffold(
         backgroundColor: Colors.white,
         body: Container(
           decoration: const BoxDecoration(
-              color: Color.fromARGB(255, 9, 175, 134),
-              borderRadius: BorderRadius.only(
-                bottomRight: Radius.circular(300),
-              )),
+            color: Color.fromARGB(255, 9, 175, 134),
+            borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(300),
+            ),
+          ),
+          child: Column(
+            children: [
+              Center(
+                child: Container(
+                  margin: const EdgeInsets.only(top: 70),
+                  height: 150,
+                  width: 150,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(300),
+                    ),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'GO',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 9, 175, 134),
+                        fontSize: 105,
+                        fontWeight: FontWeight.bold,
+                        overflow: TextOverflow.clip,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: AlignmentDirectional.centerEnd,
+                child: Container(
+                  margin: const EdgeInsets.only(right: 80),
+                  width: 70,
+                  height: 70,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(300),
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: AlignmentDirectional.centerEnd,
+                child: Container(
+                  margin: const EdgeInsets.only(right: 150),
+                  width: 30,
+                  height: 30,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(300),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              SizedBox(
+                width: 350,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'LOGIN',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 9, 175, 134),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              SizedBox(
+                width: 350,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'REGISTER',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 9, 175, 134),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
