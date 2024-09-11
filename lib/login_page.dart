c6763a11d5a20dd6b2bf72d15cf06e2fb487b675
Taoheedoh <taoheedoh@gmail.com>
2024-09-11 12:43:01 +0100

    add ButtonIcon to login

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,10 +10,46 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      backgroundColor:const Color.fromARGB(255, 9, 175, 134),
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 9, 175, 134),
       body: Container(
-
+        margin: const EdgeInsets.only(top: 100),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(35),
+            topRight: Radius.circular(35),
+          ),
+        ),
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(8),
+            ),
+            Container(
+              // width: 10,
+              // height: ,
+              // decoration: const BoxDecoration(
+              //   color: Color.fromARGB(255, 9, 175, 134),
+              //   borderRadius: BorderRadius.all(
+              //     Radius.circular(100),
+              //   ),
+              // ),
+              alignment: FractionalOffset.topLeft,
+              child: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(Icons.clear),
+                style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(
+                    Color.fromARGB(255, 9, 175, 134),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
