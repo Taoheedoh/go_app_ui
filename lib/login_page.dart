32404c782636d7eaa1a4b34d6bf4b5a19b4da5c5
Taoheedoh <taoheedoh@gmail.com>
2024-09-22 12:30:14 +0100

    Add SingleChildScrollView

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -22,137 +22,141 @@ class _LoginState extends State<Login> {
             topRight: Radius.circular(35),
           ),
         ),
-        child: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.all(8),
-            ),
-            Container(
-              margin: const EdgeInsets.only(),
-              padding: const EdgeInsets.all(10),
-              alignment: FractionalOffset.topLeft,
-              child: IconButton(
-                onPressed: () {
-                  Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(
-                      builder: (BuildContext context) => const LoginReg(),
-                    ),
-                  );
-                },
-                icon: const Icon(Icons.clear),
-                style: const ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll(
-                    Color.fromARGB(255, 9, 175, 134),
-                  ),
-                  foregroundColor: MaterialStatePropertyAll(
-                    Colors.white,
-                  ),
-                ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(8),
               ),
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            Center(
-              child: Container(
-                margin: const EdgeInsets.only(top: 0),
-                height: 100,
-                width: 100,
-                decoration: const BoxDecoration(
-                  color: Color.fromARGB(255, 9, 175, 134),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(300),
-                  ),
-                ),
-                child: const Center(
-                  child: Text(
-                    'LOGIN',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      overflow: TextOverflow.clip,
+              Container(
+                margin: const EdgeInsets.only(),
+                padding: const EdgeInsets.all(10),
+                alignment: FractionalOffset.topLeft,
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                        builder: (BuildContext context) => const LoginReg(),
+                      ),
+                    );
+                  },
+                  icon: const Icon(Icons.clear),
+                  style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(
+                      Color.fromARGB(255, 9, 175, 134),
+                    ),
+                    foregroundColor: MaterialStatePropertyAll(
+                      Colors.white,
                     ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            Column(
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(left: 10, right: 10),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'EMAIL',
-                      labelStyle: const TextStyle(
-                        color: Color.fromARGB(255, 9, 175, 134),
-                      ),
-                      prefixIcon: const Icon(
-                        Icons.mail,
-                        color: Color.fromARGB(255, 9, 175, 134),
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50),
-                        borderSide: const BorderSide(
-                            color: Color.fromARGB(255, 9, 175, 134), width: 2),
-                      ),
+              const SizedBox(
+                height: 50,
+              ),
+              Center(
+                child: Container(
+                  margin: const EdgeInsets.only(top: 0),
+                  height: 100,
+                  width: 100,
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 9, 175, 134),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(300),
                     ),
                   ),
-                ),
-
-                const SizedBox(
-                  height: 20,
-                ),
-                // ignore: avoid_unnecessary_containers
-                Container(
-                  margin: const EdgeInsets.only(left: 10, right: 10),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'PASSWORD',
-                      labelStyle: const TextStyle(
-                        color: Color.fromARGB(255, 9, 175, 134),
-                      ),
-                      prefixIcon: const Icon(
-                        Icons.lock,
-                        color: Color.fromARGB(255, 9, 175, 134),
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50),
-                        borderSide: const BorderSide(
-                            color: Color.fromARGB(255, 9, 175, 134), width: 2),
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 70,
-                ),
-
-                SizedBox(
-                  width: 370,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.all(15),
-                      side: const BorderSide(color: Colors.white, width: 2),
-                      backgroundColor: const Color.fromARGB(255, 9, 175, 134),
-                    ),
-                    child: const Text(
+                  child: const Center(
+                    child: Text(
                       'LOGIN',
                       style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
                         color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        overflow: TextOverflow.clip,
                       ),
                     ),
-                    onPressed: () {},
                   ),
                 ),
-              ],
-            ),
-          ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(left: 10, right: 10),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: 'EMAIL',
+                        labelStyle: const TextStyle(
+                          color: Color.fromARGB(255, 9, 175, 134),
+                        ),
+                        prefixIcon: const Icon(
+                          Icons.mail,
+                          color: Color.fromARGB(255, 9, 175, 134),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50),
+                          borderSide: const BorderSide(
+                              color: Color.fromARGB(255, 9, 175, 134),
+                              width: 2),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  // ignore: avoid_unnecessary_containers
+                  Container(
+                    margin: const EdgeInsets.only(left: 10, right: 10),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: 'PASSWORD',
+                        labelStyle: const TextStyle(
+                          color: Color.fromARGB(255, 9, 175, 134),
+                        ),
+                        prefixIcon: const Icon(
+                          Icons.lock,
+                          color: Color.fromARGB(255, 9, 175, 134),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50),
+                          borderSide: const BorderSide(
+                              color: Color.fromARGB(255, 9, 175, 134),
+                              width: 2),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 70,
+                  ),
+
+                  SizedBox(
+                    width: 370,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.all(15),
+                        side: const BorderSide(color: Colors.white, width: 2),
+                        backgroundColor: const Color.fromARGB(255, 9, 175, 134),
+                      ),
+                      child: const Text(
+                        'LOGIN',
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
